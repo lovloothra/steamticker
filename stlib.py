c3ac9206ed540d1919e722752e397ed235fc47e1
Lov Loothra <lovloothra@users.noreply.github.com>
2013-09-09 00:07:27 +0530

    Minor edits
    Improved query formatting. Handled exception for queries which return no values.

--- stlib.py
+++ stlib.py
@@ -10,14 +10,12 @@ def read_prices(src):
     return re.findall('&#36;(.*?) USD', src)
 
 def get_prices(query):
-    query = query.replace(" ", "+").replace("&", "%26")
+    query = query.replace(" ", "+").replace("&", "%26").replace("(", "%28").replace(")", "%29")
     src = urllib2.urlopen("http://steamcommunity.com/market/search/render/?query={0}&start=0".format(query)).read()
     return zip(read_names(src), read_prices(src))
 
 def get_price(query):
-    return get_prices(query)[0][1]
-
-def get_first_price(query):
-    query = query.replace(" ", "+").replace("&", "%26")
-    src = urllib2.urlopen("http://steamcommunity.com/market/search/render/?query={0}&start=0&count=1".format(query)).read()
-    return zip(read_names(src), read_prices(src))
+    try:
+        return get_prices(query)[0][1]
+    except:
+        return False
